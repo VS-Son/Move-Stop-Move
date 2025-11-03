c358352644d3d1da0a86de419d83dc67cf0b590d
Van Son <ngoson.200304@gmail.com>
2025-11-03 17:53:51 +0700

    add script camera follow

---
+++ Move Stop Move/Assets/Project/Scripts/Camera/CameraFollow.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField] private Transform target;
+    [SerializeField] private Vector3 offset;
+    [SerializeField] private Vector3 rotation;
+    [SerializeField] private float speed;
+
+    private void Start()
+    {
+        target = FindObjectOfType<Player>().transform;
+    }
+    private void FixedUpdate()
+    {
+        transform.position = Vector3.Lerp(transform.position, target.position + offset, Time.fixedDeltaTime * speed);
+        transform.rotation = Quaternion.Euler(rotation);
+    }
+}
