0d971052c4c4a50171d1b9f0df0001289f4bf192
Van Son <ngoson.200304@gmail.com>
2025-11-10 15:33:48 +0700

    fix character collision

--- Move Stop Move/Assets/Project/Scripts/Character/Player.cs
+++ Move Stop Move/Assets/Project/Scripts/Character/Player.cs
@@ -1,27 +1,38 @@
+using System;
 using UnityEngine;
 public class Player : Character
 {
+    private bool _isMoving = false;
     private void Update()
     {
-        if (HasEnemyInRange())
-        {
-            ChangeAnim("attack");
-        }
         if (Input.GetMouseButton(0))
         {
-            Vector3 nextPoint = JoystickControl.direct * speed * Time.deltaTime + TF.position;
+            _isMoving = true;
+           
+            
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            _isMoving = false;
+            ChangeAnim(HasEnemyInRange() ? "attack" : "idle");
+        }
+
+    
+    }
+
+    private void FixedUpdate()
+    {
+        if (_isMoving)
+        {
+            Vector3 nextPoint = JoystickControl.direct * speed * Time.fixedDeltaTime + TF.position;
             TF.position = CheckGround(nextPoint);
             if (JoystickControl.direct != Vector3.zero)
             {
                 skin.forward = JoystickControl.direct;
                 ChangeAnim("run");
             }
-            
         }
-        if (Input.GetMouseButtonUp(0))
-        {
-            ChangeAnim("idle");
-        }
+       
     }
-    
 }
